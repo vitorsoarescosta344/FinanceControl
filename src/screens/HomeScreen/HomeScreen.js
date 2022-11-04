c6603651f5ae40c1af96a628049664529887c149
Victor Soares <vitorsoarescosta344@gmail.com>
2022-11-04 20:02:36 -0300

    handle add

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,45 @@
 import {FAB, useTheme} from '@rneui/themed';
+import {useCallback, useMemo, useState} from 'react';
 import {FlatList, View} from 'react-native';
 import ChartComponent from '../../components/ChartComponent';
 import ExpensesListItem from '../../components/ExpensesListItem';
 import ModalAddExpenses from '../../components/ModalAddExpenses';
 import Container from '../../layout/Container';
+import {FinancesRealmContext} from '../../models';
+import {Finances} from '../../models/Finances';
+
+const {useQuery, useRealm} = FinancesRealmContext;
 
 export default function HomeScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
   const {theme} = useTheme();
+
+  const realm = useRealm();
+
+  const result = useQuery(Finances);
+
+  const finances = useMemo(() => result.sorted('createdAt'), [result]);
+
+  const handleDeleteFinances = useCallback(
+    finance => {
+      realm.write(() => {
+        realm.delete(finance);
+      });
+    },
+    [realm],
+  );
+
+  const handleAddFinances = useCallback(
+    (name, type, category, value) => {
+      realm.write(() => {
+        realm.create(
+          'Finances',
+          Finances.generate(name, type, category, value),
+        );
+      });
+    },
+    [realm],
+  );
 
   return (
     <>
@@ -29,7 +62,7 @@ export default function HomeScreen() {
         icon={{name: 'add', color: 'white'}}
         color={theme.colors.primary}
       />
-      <ModalAddExpenses visible={true} />
+      <ModalAddExpenses visible={true} onSubmit={handleAddFinances} />
     </>
   );
 }
