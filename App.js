58e5f0824b4eb706c18d6b19b7298240ee99c496
Victor Soares <vitorsoarescosta344@gmail.com>
2022-11-04 17:38:54 -0300

    realm context

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {ThemeProvider, createTheme} from '@rneui/themed';
+import {FinancesRealmContext} from './src/models';
 import ChartComponent from './src/components/ChartComponent';
 import HomeScreen from './src/screens/HomeScreen';
 
 const theme = createTheme({});
 
 export default function App() {
+  const {RealmProvider} = FinancesRealmContext;
+
   return (
-    <ThemeProvider theme={theme}>
-      <HomeScreen />
-    </ThemeProvider>
+    <RealmProvider>
+      <ThemeProvider theme={theme}>
+        <HomeScreen />
+      </ThemeProvider>
+    </RealmProvider>
   );
 }
