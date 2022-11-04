aa49285518bab7811ce528804dff8d5384fe0519
Victor Soares <vitorsoarescosta344@gmail.com>
2022-11-04 16:38:03 -0300

    fab

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,20 @@
+import {FAB, useTheme} from '@rneui/themed';
 import ChartComponent from '../../components/ChartComponent';
 import Container from '../../layout/Container';
 
 export default function HomeScreen() {
+  const {theme} = useTheme();
+
   return (
-    <Container>
-      <ChartComponent />
-    </Container>
+    <>
+      <Container>
+        <ChartComponent />
+      </Container>
+      <FAB
+        placement="right"
+        icon={{name: 'add', color: 'white'}}
+        color={theme.colors.primary}
+      />
+    </>
   );
 }
